c4dcf4dbc45f2d73df3b2463f49074a9a7e8b3a6
ShovelSquid <kaelen1cook@gmail.com>
2019-04-22 09:35:48 -0700

    Basic Movement
    Created a small website that allows player to control an image.
    Able to move 4 ways with either arrow keys or WASD

---
+++ branch.js
@@ -0,0 +1,79 @@
+const player = document.getElementById("sprite");
+let left = 0;
+let top_ = 0;
+
+
+
+function KeyPresser(e) {
+	var key_code= e.which || e.keyCode;
+	switch(key_code) {
+		case 37: //leftarrow
+			PlayerMoveLeft();
+			break;
+		case 38: //uparrow
+			PlayerMoveUp();
+			break;
+		case 39: //rightarrow
+			PlayerMoveRight();
+			break;
+		case 40: //downarrow
+			PlayerMoveDown();
+			break;
+
+			//WASD
+
+		case 65: //a key (left)
+			PlayerMoveLeft();
+			break;
+		case 87: //w key (up)
+			PlayerMoveUp();
+			break;
+		case 68: //d key (right)
+			PlayerMoveRight();
+			break;
+		case 83: //s key (down)
+			PlayerMoveDown();
+			break;
+	}
+}
+
+
+function PlayerMoveLeft() {
+	if (left == 0) {
+		player.style.left = 0 + "px"
+	}
+	else {
+		left = left - 50;
+		player.style.left = left + "px"
+	}
+}
+
+function PlayerMoveRight() {
+	if (left == 450) {
+		player.style.left = 450 + "px"
+	}
+	else {
+		left += 50;
+		player.style.left = left + "px"
+	}
+}
+
+function PlayerMoveUp() {
+	if (top_ == 0) {
+		player.style.top = 0 + "px"
+	}
+	else {
+		top_ = top_ - 50;
+		player.style.top = top_ + "px"
+	}
+}
+
+function PlayerMoveDown() {
+	if (top_ == 450) {
+		player.style.top = 450 + "px"
+	}
+	else {
+		top_ += 50;
+		player.style.top = top_ + "px"
+	}
+}
