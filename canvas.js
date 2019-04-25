ceca40097ae408d35e7e6b0dbcd31b381166817f
swerze <reedsonofwill@gmail.com>
2019-04-25 08:49:42 -0700

    idk

--- canvas.js
+++ canvas.js
@@ -3,5 +3,6 @@ var HTML = function(canvasID, width, height) {
 	this.height = height;
 	this.canvas = null;
 	this.context = null;
-	
+	this.canvas = document.getElementById(canvas_tag_id);
+	this.context = this.canvas.getContext('2d');
 }
